5db3889ce2cdff49d1eaa535aa4ddae05f5913dc
Dipika Gautam <dipikagautam001@gmail.com>
2025-08-09 12:16:55 +0530

    Time: 1 ms (35.75%), Space: 41.1 MB (36.36%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,17 +1,22 @@
 class Solution {
-
-    public boolean rec(int n){
-
-        if(n<=0) return false;
-        if(n==1) return true;
-
-        if(n%2!=0) return false;
-        return rec(n/2);
-    }
     public boolean isPowerOfTwo(int n) {
-        return rec(n);
+           if (n == 1)
+            return true;
 
+        if (n == 0)
+            return false;
 
-        
+        while (n != 1) {
+
+            if (n % 2 != 0) {
+                return false;
+            } else {
+
+                n = n / 2;
+            }
+        }
+
+        return true;
+
     }
 }

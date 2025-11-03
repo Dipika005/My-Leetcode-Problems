04b8db3e7434300ebd7d1a84e56edd86b10d8676
Dipika Gautam <dipikagautam001@gmail.com>
2025-11-03 21:23:30 +0530

    Time: 5 ms (51.34%), Space: 46.2 MB (6.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,13 +3,21 @@ class Solution {
         
         int n=nums.length;
         int[] ans={-1};
+
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[j]==target-nums[i]){
-                    ans=new int[]{i,j};
-                }
+            if(!hm.containsKey(target-nums[i])){
+                hm.put(nums[i],i);
+            }
+            else{
+                ans=new int[]{i,hm.get(target-nums[i])};
             }
         }
         return ans;
+
     }
 }
+
+// TC : O(N)
+// SC : O(N)

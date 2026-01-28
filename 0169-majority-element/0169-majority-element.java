a2f3627552520c17d8948498640fb88faf3468a7
Dipika Gautam <dipikagautam001@gmail.com>
2026-01-28 16:00:53 +0530

    Time: 2 ms (71.9%), Space: 56.1 MB (13.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n= nums.length;
-        Arrays.sort(nums);
-        return nums[n/2];
+        // Moore’s Voting Algorithm
+
+        int ele=0;
+        int cnt=0;
+
+       for(int i=0;i<nums.length;i++){
+        if(cnt==0){
+            ele=nums[i];
+        }
+        if(nums[i]==ele) cnt++;
+        else cnt--;
+       }
+
+       // checking 
+       int finalcnt=0;
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]==ele) finalcnt++;
+       }
+
+       if(finalcnt>nums.length/2) return ele;
+       return -1;
     }
 }

e5e1d42a7ee647a00251c77027f00a4eb67cb235
Dipika Gautam <dipikagautam001@gmail.com>
2026-01-28 16:39:20 +0530

    Time: 1 ms (99.59%), Space: 77.3 MB (40.7%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max= Integer.MIN_VALUE;
-        int sum = 0;
+        
+        // KADANE'S ALGO
 
-        for(int i=0;i<nums.length;i++){
-            if(sum<0){
-                sum=0;
-            }
-            sum+=nums[i];
-            max= Math.max(max,sum);
+        int max=nums[0];
+        int currSum=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            currSum=Math.max(nums[i],currSum+nums[i]);
+            max=Math.max(currSum,max);
         }
         return max;
-        
     }
 }

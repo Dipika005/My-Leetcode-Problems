d966ee7700db7ce6ae390da06b526732142b1888
Dipika Gautam <dipikagautam001@gmail.com>
2026-01-30 19:26:41 +0530

    Time: 8 ms (49.71%), Space: 58.5 MB (5%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,55 +1,21 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-
-        // Boyer Moore Voting Algo Start
-        int num1 = -1;
-        int num2 = -1;
-        int count1 = 0;
-        int count2 = 0;
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == num1)
-                count1++;
-
-            else if (nums[i] == num2)
-                count2++;
-
-            else if (count1 == 0) {
-                num1 = nums[i];
-                count1 = 1;
+        List<Integer> ans=new ArrayList<>();
+        int n=nums.length;
+        Arrays.sort(nums);
+        int cnt=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]){
+                cnt++;
             }
-
-            else if (count2 == 0) {
-                num2 = nums[i];
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
-            }
+            else{
+                if(cnt>(n/3)) ans.add(nums[i-1]);
+                cnt=1;
+            }   
         }
-        //End
-
-        List<Integer> ans = new ArrayList<Integer>();
-
-        int cnt1=0;
-        int cnt2=0;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == num1)
-                cnt1++;
-
-            else if (nums[i] == num2)
-                cnt2++;
-
-        }
-        if (cnt1 > n / 3) {
-            ans.add(num1);
-        }
-        if (cnt2 > n / 3) {
-            ans.add(num2);
-        }
+         // check last element group
+        if (cnt > n / 3)
+            ans.add(nums[n - 1]);
         return ans;
-
     }
 }

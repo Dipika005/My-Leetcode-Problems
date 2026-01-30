81707b6f313dadca41207bd2941d0bec0a3e1c2e
Dipika Gautam <dipikagautam001@gmail.com>
2026-01-30 20:04:17 +0530

    Time: 2 ms (99.58%), Space: 52.9 MB (24.3%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,20 +2,37 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> ans=new ArrayList<>();
         int n=nums.length;
-        Arrays.sort(nums);
-        int cnt=1;
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
-                cnt++;
+        int c1=0, e1=Integer.MIN_VALUE;
+        int c2=0, e2=Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            if (nums[i] == e1) {
+                c1++;
             }
-            else{
-                if(cnt>(n/3)) ans.add(nums[i-1]);
-                cnt=1;
-            }   
+            else if (nums[i] == e2) {
+                c2++;
+            }
+            else if (c1 == 0) {
+                e1 = nums[i];
+                c1 = 1;
+            }
+            else if (c2 == 0) {
+                e2 = nums[i];
+                c2 = 1;
+            }
+            else {
+                c1--;
+                c2--;
+            }
         }
-         // check last element group
-        if (cnt > n / 3)
-            ans.add(nums[n - 1]);
+        int cnt1=0;
+        int cnt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==e1) cnt1++;
+            if(nums[i]==e2) cnt2++; 
+        } 
+        if(cnt1>n/3) ans.add(e1);
+        if(cnt2>n/3) ans.add(e2);
         return ans;
     }
 }

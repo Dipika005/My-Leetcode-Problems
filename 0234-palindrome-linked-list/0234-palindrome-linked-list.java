43fe35f87ac26339502181ef715a65401e504dbb
Dipika Gautam <dipikagautam001@gmail.com>
2026-02-14 22:12:47 +0530

    Time: 4 ms (75.54%), Space: 96.9 MB (31.1%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -33,7 +33,7 @@ class Solution {
         ListNode fast = head;
         ListNode slow = head;
 
-        while(fast.next.next!=null && fast.next!=null){
+        while(fast.next!=null && fast.next.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
